7afcc1baf798d0a3fcf3730eaf83dede3bc30345
DiogoGomes88216 <t3diogogomes@hotmail.com>
2022-05-06 18:19:24 +0100

    New Division and Ground Classification

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/common/common.h>
-#include <pcl/filters/extract_indices.h>
 
-#define grid_size 100
+#include "groundSeg.h"
+
+#define grid_size 1.0f
 
 ros::Publisher pub;
 
@@ -18,41 +18,13 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>());
     pcl::PointCloud<pcl::PointXYZI> cloud_filtered;
 
-    pcl::IndicesPtr inliers (new pcl::Indices()), tmp_inliers (new pcl::Indices()) ;
-
-    Eigen::Vector4f max_pt, min_pt;
-
-    // Create the filtering object
-    pcl::ExtractIndices<pcl::PointXYZI> extract;
-
     //Convert Point Cloud 
     pcl::fromROSMsg(*input, *cloud);
 
-    //Filter
-    pcl::getMinMax3D(*cloud, min_pt, max_pt);
-
-    const float max_y = max_pt.y(), max_x = max_pt.y(), min_y = min_pt.y(), min_x = min_pt.x();
-
-    for(float j = max_y; j > min_y ; j -= grid_size)
-    {
-        max_pt.y() = j;
-        min_pt.y() = max_pt.y() - grid_size;
-
-        for(float i = min_x; i < max_x ; i += grid_size)
-        {
-            min_pt.x() = i;
-            max_pt.x() = min_pt.x() + grid_size;
-            
-            pcl::getPointsInBox(*cloud, min_pt, max_pt, *tmp_inliers);
-            inliers->insert( inliers->end(), tmp_inliers->begin(), tmp_inliers->end() );
-        }
-    } 
-       
-    // Extract the inliers
-    extract.setInputCloud (cloud);
-    extract.setIndices(inliers);
-    extract.setNegative (false);
-    extract.filter(cloud_filtered);
+    //Apply Filter
+    pcl::GroundSeg gs(grid_size);
+    gs.setInputCloud(cloud);
+    gs.filter(cloud_filtered);
 
     // Back to sensor_msgs
     pcl::toROSMsg(cloud_filtered, output);
